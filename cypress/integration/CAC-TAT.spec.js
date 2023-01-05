0209ac010f1a1458e240a4fad1c6d5baa9f0ac66
adrieliket <adrielikethin.dossantos@gmail.com>
2023-01-05 13:37:22 -0300

    Exercio extra 1 da sessão 3 (delay)

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,12 +10,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
         cy.get('#firstName').type('Adrieli');
         cy.get('#lastName').type('Santos');
         cy.get('#email').type('adrieli@gmail.com');
         cy.get('#phone').type('999999999');
-        cy.get('#open-text-area').type('Testes')
+        cy.get('#open-text-area').type(longText, {delay: 0})
         cy.get('.button[type="submit"]').click();
         cy.get('.success').should('be.visible');
     })
