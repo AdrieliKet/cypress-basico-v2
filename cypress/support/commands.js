c9ecd8f3caba46a8299eb2fbd4ec6bbaaf70545b
adrieliket <adrielikethin.dossantos@gmail.com>
2023-01-05 14:37:52 -0300

    Impedindo vazamento de dados na execução dos testes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,8 @@
 Cypress.Commands.add("fillMandatoryFieldsAndSubmit", function(firtsName, lastName, email, phone, textArea) {
-    cy.get('#firstName').type(firtsName);
-        cy.get('#lastName').type(lastName);
-        cy.get('#email').type(email);
-        cy.get('#phone').type(phone);
-        cy.get('#open-text-area').type(textArea);
+    cy.get('#firstName').type(firtsName, { log: false });
+        cy.get('#lastName').type(lastName, { log: false });
+        cy.get('#email').type(email, { log: false });
+        cy.get('#phone').type(phone, { log: false });
+        cy.get('#open-text-area').type(textArea, { log: false });
         cy.get('.button[type="submit"]').click();
 })
