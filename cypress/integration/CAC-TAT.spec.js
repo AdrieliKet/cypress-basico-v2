3daad1fb175f5ec2a697a26fd5601bf90ae52cb1
adrieliket <adrielikethin.dossantos@gmail.com>
2023-01-05 15:15:59 -0300

    Exercico da seção 6 (check uncheck checkboxes)

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -81,10 +81,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('input[type="radio"][value="feedback"]').check().should('have.value', 'feedback')
     })
 
-    it.only('marca cada tipo de atendimento', function() {
+    it('marca cada tipo de atendimento', function() {
         cy.get('input[type="radio"]').should('have.length', 3).each(function($radio) {
             cy.wrap($radio).check();
             cy.wrap($radio).should('be.checked');
         })
+    })
+
+    it.only('marca ambos checkboxes, depois desmarca o último', function() {
+        cy.get('input[type="checkbox"]').check().should('be.checked').last().uncheck().should('not.be.checked');
     })
   })
