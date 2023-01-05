5f511edb5722bd9291a13382c742643e438070d2
adrieliket <adrielikethin.dossantos@gmail.com>
2023-01-05 11:57:33 -0300

    Exercicio da sessão 3 do curso de cypress basico

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    beforeEach(function() {
+        cy.visit('./src/index.html');
+    })
+
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+        cy.get('#firstName').type('Adrieli');
+        cy.get('#lastName').type('Santos');
+        cy.get('#email').type('adrieli@gmail.com');
+        cy.get('#phone').type('999999999');
+        cy.get('#open-text-area').type('Testes')
+        cy.get('.button[type="submit"]').click();
+        cy.get('.success').should('be.visible');
+    })
+  })
