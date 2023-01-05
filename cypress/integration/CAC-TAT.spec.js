f8a4e54a7d8877bfa52d8aab8baec9aa0958d0b8
adrieliket <adrielikethin.dossantos@gmail.com>
2023-01-05 15:01:00 -0300

    Exercicio da sessão 5 (check)

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -65,15 +65,19 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible');
     })
 
-    it.only('seleciona um produto (YouTube) por seu texto', function() {
+    it('seleciona um produto (YouTube) por seu texto', function() {
         cy.get('#product').select('YouTube').should('have.value', 'youtube')
     })
 
-    it.only('seleciona um produto (Mentoria) por seu valor (value)', function() {
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
         cy.get('#product').select('mentoria').should('have.value', 'mentoria')
     })
 
-    it.only('seleciona um produto (Blog) por seu índice', function() {
+    it('seleciona um produto (Blog) por seu índice', function() {
         cy.get('#product').select(1).should('have.value', 'blog')
+    })
+
+    it.only('marca o tipo de atendimento "Feedback"', function() {
+        cy.get('input[type="radio"][value="feedback"]').check().should('have.value', 'feedback')
     })
   })
